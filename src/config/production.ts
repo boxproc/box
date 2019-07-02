225d36dc306afe9b028b1b54275124b9ae276b20
Iryna Dmytruk <iryna.d@nerdysoft.com>
2019-07-02 14:25:25 +0300

    Fix production config.

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,6 +1,4 @@
 export default Object.freeze({
-  // apiUrl: process.env.REACT_APP_API_URL,
-  // apiHost: process.env.REACT_APP_API_HOST,
-  apiUrl: 'http://10.37.10.3:8080/boxproc',
-  apiHost: 'http://10.37.10.3:8080',
+  apiUrl: process.env.REACT_APP_API_URL,
+  apiHost: process.env.REACT_APP_API_HOST,
 });
