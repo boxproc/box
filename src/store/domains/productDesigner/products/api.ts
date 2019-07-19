22bd4e3aee3b4218c8f0c8a0e935056e284d173f
Iryna Dmytruk <iryna.d@nerdysoft.com>
2019-07-19 18:31:12 +0300

    Comment apiClient. Move Dropdown to separate folder.

--- src/store/domains/productDesigner/products/api.ts
+++ src/store/domains/productDesigner/products/api.ts
@@ -1,20 +1,20 @@
-// import { apiClient } from 'services';
+import { apiClient } from 'services';
 
-import {
-  ProductItemsData,
-  ProductItemsDataFiltered,
-} from './mock';
+// import {
+//   ProductItemsData,
+//   ProductItemsDataFiltered,
+// } from './mock';
 
 import { throttleUtil } from 'utils';
 import { ProductFilterParamsPrepared } from './types';
 
 export const getProducts = () =>
-  throttleUtil.getDataAfter(ProductItemsData, 500);
-  // apiClient.post('/ui/product_designer/products/get');
+  // throttleUtil.getDataAfter(ProductItemsData, 500);
+  apiClient.post('/ui/product_designer/products/get');
 
 export const deleteProduct = (id: number | string) =>
   throttleUtil.getDataAfter({id}, 500);
 
 export const filterProducts = (data: ProductFilterParamsPrepared) =>
-  throttleUtil.getDataAfter(ProductItemsDataFiltered, 500);
-  // apiClient.post('/ui/administration/system_properties/get', {data});
+  // throttleUtil.getDataAfter(ProductItemsDataFiltered, 500);
+  apiClient.post('/ui/administration/system_properties/get', {data});
