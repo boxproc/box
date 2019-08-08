0823bc64a5cd0b6c603c0d75cedffac338571dc1
Bohdan Prots <protsbohdan9@gmail.com>
2019-08-08 17:31:51 +0300

    Merge

--- src/store/domains/audit/userActivity/types.ts
+++ src/store/domains/audit/userActivity/types.ts
@@ -1,18 +1,32 @@
-import { ImmutableArray } from "seamless-immutable";
-import { ResponseStatusType } from "types";
+import { ImmutableArray } from 'seamless-immutable';
+import { ResponseStatusType } from 'types';
 
 export interface AuditUserActivitiesItemResp {
     id: number;
     user_id: number;
     api_name: string;
     description: string;
+    event_datetime: string;
+    institution_id: number;
+    username: string;
 }
 
 export interface AuditUserActivitiesItem {
     id: number;
+    institutionId: number;
     userId: number;
     apiName: string;
+    eventDatetime: string;
     description: string;
+    username: string;
+}
+
+export interface AuditUserActivitySelectInstitution {
+    institution_id: number | string;
+}
+export interface AuditUserActivitySelectInstitutionResponse {
+    id: number;
+    username: string;
 }
 
 export interface AuditUserActivitiesDataResp extends ResponseStatusType {
