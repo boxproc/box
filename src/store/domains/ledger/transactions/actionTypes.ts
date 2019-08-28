1f44fdbb444c0dc5d06abc91c3a89c8da911d446
Iryna Dmytruk <iryna.d@nerdysoft.com>
2019-08-28 21:16:55 +0300

    Fix transactions.

--- src/store/domains/ledger/transactions/actionTypes.ts
+++ src/store/domains/ledger/transactions/actionTypes.ts
@@ -2,30 +2,11 @@ import { ApiResponse } from 'types';
 import { LedgerTransactionItems } from './types';
 
 export enum ActionTypeKeys {
-  GET_LEDGER_TRANSACTIONS = 'ledger/transactions/GET_LEDGER_TRANSACTIONS',
-  GET_LEDGER_TRANSACTIONS_FULFILLED = 'ledger/transactions/GET_LEDGER_TRANSACTIONS_FULFILLED',
-  GET_LEDGER_TRANSACTIONS_REJECTED = 'ledger/transactions/GET_LEDGER_TRANSACTIONS_REJECTED',
-
   SET_LEDGER_TRANSACTION_ID = 'ledger/transactions/SET_LEDGER_TRANSACTION_ID',
 
   FILTER_LEDGER_TRANSACTIONS = 'ledger/transactions/FILTER_LEDGER_TRANSACTIONS',
   FILTER_LEDGER_TRANSACTIONS_FULFILLED = 'ledger/transactions/FILTER_LEDGER_TRANSACTIONS_FULFILLED',
   FILTER_LEDGER_TRANSACTIONS_REJECTED = 'ledger/transactions/FILTER_LEDGER_TRANSACTIONS_REJECTED',
-}
-
-export interface GetLedgerTransactionsAction {
-  readonly payload: Promise<object>;
-  readonly type: ActionTypeKeys.GET_LEDGER_TRANSACTIONS;
-}
-
-export interface GetLedgerTransactionsFulfilledAction {
-  readonly payload: LedgerTransactionItems;
-  readonly type: ActionTypeKeys.GET_LEDGER_TRANSACTIONS_FULFILLED;
-}
-
-export interface GetLedgerTransactionsRejectedAction {
-  readonly payload: ApiResponse;
-  readonly type: ActionTypeKeys.GET_LEDGER_TRANSACTIONS_REJECTED;
 }
 
 export interface SetLedgerTransactionIdAction {
@@ -49,6 +30,5 @@ export interface FilterLedgerTransactionsRejectedAction {
 }
 
 export type LedgerTransactionsActionTypes =
-  | GetLedgerTransactionsFulfilledAction
   | FilterLedgerTransactionsFulfilledAction
   | SetLedgerTransactionIdAction;
--- src/store/domains/ledger/transactions/api.ts
+++ src/store/domains/ledger/transactions/api.ts
@@ -2,14 +2,10 @@ import { lenderTransactionsPathNames } from 'consts';
 
 import { apiClient } from 'services';
 
-// import { LedgerTransactionsFilteredItems, ledgerTransactionsItems } from './mock';
+// import { LedgerTransactionsFilteredItems } from './mock';
 
 // import { throttleUtil } from 'utils';
 import { LedgerTransactionsFilterParamsPrepared } from './types';
-
-export const getLedgerTransactions = () =>
-  // throttleUtil.getDataAfter(ledgerTransactionsItems, 500);
-  apiClient.post(lenderTransactionsPathNames.GET);
 
 export const filterLedgerTransactions = (data: Partial<LedgerTransactionsFilterParamsPrepared>) =>
   // throttleUtil.getDataAfter(LedgerTransactionsFilteredItems, 500);
