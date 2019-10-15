81541fddf4861c005344c75df6ea73f87cae55e2
Bohdan Prots <protsbohdan9@gmail.com>
2019-10-15 16:50:25 +0300

    Fix date format (Swap yesterday and today date)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,10 +2,11 @@ import moment from 'moment';
 
 import { dateFormat as dateFormatConst } from 'consts';
 
-export const todayDateTime = () => moment().subtract(1, 'day').format(dateFormatConst.DATE_TIME);
-export const yesterdayDateTime = () => moment().format(dateFormatConst.DATE_TIME);
-export const todayDate = moment().subtract(1, 'day').format(dateFormatConst.DATE);
-export const yesterdayDate = moment().format(dateFormatConst.DATE);
+export const todayDateTime = () => moment().format(dateFormatConst.DATE_TIME);
+export const yesterdayDateTime = () =>
+  moment().subtract(1, 'day').format(dateFormatConst.DATE_TIME);
+export const todayDate = moment().format(dateFormatConst.DATE);
+export const yesterdayDate = moment().subtract(1, 'day').format(dateFormatConst.DATE);
 
 export const toFormattedCalendarDate =
   (date: string, dateFormat: string = dateFormatConst.DATE, lng: string = 'en-GB') => {
