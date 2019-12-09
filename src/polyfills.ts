b29cb9550b410eca963c9ae2322b0c7f7d7be660
Iryna Dmytruk <iryna.d@nerdysoft.com>
2019-12-09 22:32:54 +0200

    Correct polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,22 +1,3 @@
 import 'react-app-polyfill/ie11';
 
 import 'core-js';
-
-/* tslint:disable */
-(function () {
-  if (!Element.prototype.closest) {
-    Element.prototype.closest = function (css: string) {
-      let node = this;
-
-      while (node) {
-        if (node.matches(css)) {
-          return node;
-        } else {
-          node = node.parentElement;
-        }
-      }
-      return null;
-    };
-  }
-})();
-/* tslint:enable */
