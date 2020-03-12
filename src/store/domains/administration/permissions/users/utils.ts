a66dcde03cbd1b0ebe3d310731b1fd329d0c6401
Iryna Dmytruk <iryna.d@nerdysoft.com>
2020-03-12 16:33:03 +0200

    Adjust user util

--- src/store/domains/administration/permissions/users/utils.ts
+++ src/store/domains/administration/permissions/users/utils.ts
@@ -8,19 +8,34 @@ export const prepareAdminUserDataToSend = (data: Partial<AdminUserItemDetails>) 
     return null;
   }
 
+  const {
+    id,
+    username,
+    email,
+    firstName,
+    lastName,
+    password,
+    passwordEntryCounter,
+    datetimeOfLastLogin,
+    status,
+    institution,
+    requires2faFlag,
+    changeProfileAllowedFlag,
+  } = data;
+
   return {
-    id: data.id,
-    username: data.username,
-    email: data.email,
-    first_name: data.firstName,
-    last_name: data.lastName,
-    password: data.password,
-    password_entry_counter: data.passwordEntryCounter,
-    datetime_of_last_login: data.datetimeOfLastLogin,
-    status: data.status && data.status.value,
-    institution_id: data.institution.value,
-    requires_2fa_flag: data.requires2faFlag ? yesNoTypesCodes.YES : yesNoTypesCodes.NO,
-    change_profile_allowed_flag: data.changeProfileAllowedFlag
+    id,
+    username,
+    email,
+    first_name: firstName,
+    last_name: lastName,
+    password: password ? password : null,
+    password_entry_counter: passwordEntryCounter,
+    datetime_of_last_login: datetimeOfLastLogin,
+    status: status && status.value,
+    institution_id: institution && institution.value,
+    requires_2fa_flag: requires2faFlag ? yesNoTypesCodes.YES : yesNoTypesCodes.NO,
+    change_profile_allowed_flag: changeProfileAllowedFlag
       ? yesNoTypesCodes.YES
       : yesNoTypesCodes.NO,
   };
@@ -34,20 +49,32 @@ export const prepareAdminUserDataToRender = (
     return null;
   }
 
+  const {
+    id,
+    username,
+    first_name,
+    last_name,
+    email,
+    requires_2fa_flag,
+    change_profile_allowed_flag,
+    password_entry_counter,
+    datetime_of_last_login,
+  } = data;
+
   const status = status2faLoginOptions.find(el => el.value === data.status);
 
   return {
-    id: data.id,
-    username: data.username,
-    firstName: data.first_name,
-    lastName: data.last_name,
+    id,
+    username,
+    firstName: first_name,
+    lastName: last_name,
     institution: institution && institution.label,
-    email: data.email,
+    email,
     status: status && status.label,
-    requires2faFlag: data.requires_2fa_flag === yesNoTypesCodes.YES,
-    changeProfileAllowedFlag: data.change_profile_allowed_flag === yesNoTypesCodes.YES,
-    passwordEntryCounter: data.password_entry_counter,
-    datetimeOfLastLogin: data.datetime_of_last_login,
+    requires2faFlag: requires_2fa_flag === yesNoTypesCodes.YES,
+    changeProfileAllowedFlag: change_profile_allowed_flag === yesNoTypesCodes.YES,
+    passwordEntryCounter: password_entry_counter,
+    datetimeOfLastLogin: datetime_of_last_login,
   };
 };
 
